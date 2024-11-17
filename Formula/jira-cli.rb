81534384e5511af8e861eff0559d1c628cc9af4f
Mark Iannucci <mark.iannucci@gmail.com>
2024-11-17 09:38:58 -0700

    add exec

--- Formula/jira-cli.rb
+++ Formula/jira-cli.rb
@@ -45,7 +45,7 @@ class JiraCli < Formula
     else
       bin.install File.exist?("bin/jira") ? "bin/jira" : "jira"
     end
-    jira completion bash > ./bash_completion
+    exec "jira completion bash > ./bash_completion"
     bash_completion.install "./bash_completion"
     # system "jira" , "completion", "bash" , ">", "./bash_completion"
     # bash_completion.install "./bash_completion"
